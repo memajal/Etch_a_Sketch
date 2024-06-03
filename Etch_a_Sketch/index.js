a0a14bb97a85ed2fd115d64caac3988d3db78176
memajal <memajalketa92@gmail.com>
2024-06-03 23:14:48 +0200

    Add a parameter to the function createContainerGrid that will contain the numer of boxes enter by the user.
    Add a parameter to the createContainerGrid function, include the eventlistener onmouseover to the function, call the function with parameter 16.

--- Etch_a_Sketch/index.js
+++ Etch_a_Sketch/index.js
@@ -1,40 +1,33 @@
+let container=document.querySelector("#container");
 
-let conteiner=document.querySelector("#container");
 
-
-function createContainerGrid(numberOfBox){
-    for(let i=0; i<numberOfBox; i++) {
+function createContainerGrid(numberOfBoxes){
+    for(let i=0; i<numberOfBoxes; i++) {
         const gridRow=document.createElement("div");
-        gridRow.className="grid-row";
-    
-        for(let j=0; j<numberOfBox; j++) {
+        gridRow.className="grid_row";
+        gridRow.setAttribute("style", "display: flex;flex-grow: 1;");
+        
+
+        for(let j=0; j<numberOfBoxes; j++) {
+
         const gridBox=document.createElement("div");
-        gridBox.className="grid-box";
+        gridBox.className="grid_box";
+        gridBox.setAttribute("style", "flex-grow: 1;border: 2px solid black;");
         gridRow.appendChild(gridBox);
+
+        gridBox.addEventListener("mouseover", ()=> {
+            gridBox.style.backgroundColor = 'blue'; });
         }
-    
-        conteiner.appendChild(gridRow);
+
+        container.appendChild(gridRow);
         }    
     }
 
     createContainerGrid(16);
-    const conteinerBox= document.querySelectorAll(".grid-box");
 
-    function onMouseOverEffect(){
-        conteinerBox.forEach((box) =>{
-            // add a click listener
-                box.addEventListener("mouseover", ()=> {
-                    box.style.backgroundColor = 'blue'; 
-            });  
-        });
-    }
 
     function numberofSuqares(){
         let numberofSuqares= prompt
         ("Enter number of squares from 16 to 100");
         console.log(numberofSuqares);
     }
-
-
-
-onMouseOverEffect();
