54307b0106f20d75e2ecafcff578701298abd8fa
memajal <memajalketa92@gmail.com>
2024-06-03 18:55:44 +0200

    Changing the class of grid row and box and adding the intitial style for 16x16 grid to style.css

--- Etch_a_Sketch/index.js
+++ Etch_a_Sketch/index.js
@@ -1,26 +1,24 @@
-let numberOfRows=16;
-let numberofColumns=16;
+
 let conteiner=document.querySelector("#container");
 
 
-function createContainerGrid(){
-    for(let i=0; i<16; i++) {
-        const conteinerBoxVertical=document.createElement("div");
-        conteinerBoxVertical.className="conteiner_box_vertical";
-        conteinerBoxVertical.setAttribute("style", "border:2px solid red; width:30px; height:auto;");
-        conteiner.appendChild(conteinerBoxVertical);
-
-        for(let j=0; j<16; j++) {
-            const conteinerBox=document.createElement("div");
-        conteinerBox.className="conteiner_box";
-        conteinerBox.setAttribute("style", "border:2px solid red; width:auto; height:30px;");
-        conteinerBoxVertical.appendChild(conteinerBox);
+function createContainerGrid(numberOfBox){
+    for(let i=0; i<numberOfBox; i++) {
+        const gridRow=document.createElement("div");
+        gridRow.className="grid-row";
+    
+        for(let j=0; j<numberOfBox; j++) {
+        const gridBox=document.createElement("div");
+        gridBox.className="grid-box";
+        gridRow.appendChild(gridBox);
         }
+    
+        conteiner.appendChild(gridRow);
         }    
     }
 
-    createContainerGrid();
-    const conteinerBox= document.querySelectorAll(".conteiner_box");
+    createContainerGrid(16);
+    const conteinerBox= document.querySelectorAll(".grid-box");
 
     function onMouseOverEffect(){
         conteinerBox.forEach((box) =>{
