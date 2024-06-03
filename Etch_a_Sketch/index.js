2e6f4bd0393dd2d9eaaa547b3eff4ac2a683db76
memajal <memajalketa92@gmail.com>
2024-06-03 10:33:08 +0200

    Function onMouseOverEffect gives a background color to conteinerBox, every time the mouse is over it

--- Etch_a_Sketch/index.js
+++ Etch_a_Sketch/index.js
@@ -9,19 +9,28 @@ function createContainerGrid(){
         conteinerBoxVertical.className="conteiner_box_vertical";
         conteinerBoxVertical.setAttribute("style", "border:2px solid red; width:30px; height:auto;");
         conteiner.appendChild(conteinerBoxVertical);
+
         for(let j=0; j<16; j++) {
             const conteinerBox=document.createElement("div");
         conteinerBox.className="conteiner_box";
         conteinerBox.setAttribute("style", "border:2px solid red; width:auto; height:30px;");
         conteinerBoxVertical.appendChild(conteinerBox);
-
         }
-       
-            }    
+        }    
+    }
+
+    createContainerGrid();
+    const conteinerBox= document.querySelectorAll(".conteiner_box");
+
+    function onMouseOverEffect(){
+        conteinerBox.forEach((box) =>{
+            // add a click listener
+                box.addEventListener("mouseover", ()=> {
+                    box.style.backgroundColor = 'blue'; 
+            });  
+        });
     }
 
 
 
-
-
-createContainerGrid();
+onMouseOverEffect();
